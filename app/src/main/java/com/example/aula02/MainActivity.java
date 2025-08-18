6949a0cbc61555906a9a46fb5558697099af9f79
natan <natan.jv>
2025-08-18 08:52:50 -0300

    Contador

--- app/src/main/java/com/example/aula02/MainActivity.java
+++ app/src/main/java/com/example/aula02/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aula02;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView;
 
+    int contador;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +25,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.tv);
-        textView.setText("Me mata senhor");
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText(contadror);
-            }
+        textView.setText(Integer.toString(contador));
+        textView.setOnClickListener(v -> {
+            contador++;
+            textView.setText(Integer.toString(contador));
         });
 
     }
