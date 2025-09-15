6296acf720edc80e677e4b01ab474423fd0a3627
natVansuita <natanjoaovansuita@gmail.com>
2025-09-15 09:13:07 -0300

    telas de resultados01

--- app/src/main/java/com/example/aula02/IMCresultados.java
+++ app/src/main/java/com/example/aula02/IMCresultados.java
@@ -1,5 +1,6 @@
 package com.example.aula02;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +16,19 @@ public class IMCresultados extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_imcresultados);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        tvResultado = findViewById(R.id.tvResultado);
+        imageView2 = findViewById(R.id.imageView2);
+
+
+        //recuperando o intent
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        Double peso = b.getDouble("peso");
+        Double altura = b.getDouble("altura");
+
+        //calculo do imc
+        Double imc = peso/(altura*altura);
+
+        tvRe
     }
 }
--- app/src/main/java/com/example/aula02/MainActivity.java
+++ app/src/main/java/com/example/aula02/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.aula02;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +13,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText edPeso,edAltura;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        button.setOnClickListener( V -> {
+        button = findViewById(R.id.button);
+        edPeso = findViewById(R.id.TTkg);
+        edAltura = findViewById(R.id.TTm);
 
+        button.setOnClickListener(V -> {
+            Intent i = new Intent(MainActivity.this,MainActivity.class);
+            Bundle bundle = new Bundle();
+
+            double peso = Double.parseDouble(edPeso.getText().toString());
+            bundle.putDouble("peso",peso);
+
+            double altura = Double.parseDouble(edAltura.getText().toString());
+            bundle.putDouble("altura",altura);
+
+            i.putExtras(bundle);
+
+            startActivity(i);
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
