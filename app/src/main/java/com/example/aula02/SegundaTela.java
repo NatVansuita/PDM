3260a4f2336f8783b3e7f06ff1428fc6cd639298
natVansuita <natanjoaovansuita@gmail.com>
2025-09-22 09:04:08 -0300

    tabuada

--- app/src/main/java/com/example/aula02/SegundaTela.java
+++ app/src/main/java/com/example/aula02/SegundaTela.java
@@ -1,6 +1,5 @@
 package com.example.aula02;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,23 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Bundle bundle;
+public class SegundaTela extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_primeira_tela);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
 
-        var mudarTela = new Intent(this, SegundaTela.class);
-    Bundle bundle;
-    startActivity(mudarTela);
+        });
     }
 }
