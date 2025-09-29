3539b48c48951ed782e5c2e44022b7589256a7f1
natVansuita <natanjoaovansuita@gmail.com>
2025-09-29 09:02:38 -0300

    Listagem.01

--- app/src/main/java/com/example/aula02/MainActivity.java
+++ app/src/main/java/com/example/aula02/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula02;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    String [] nomes = new String[] {"Natan", "Sarah", "Maria", "Anaju", "Romulo", "Clara"};
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.list);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, nomes);
+
+        listView.setAdapter(adapter);
     }
 }
