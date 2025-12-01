98c04acc951e8f4b89c3467ff82e9a7bf760018d
Natan J. Vansuita <natanjoaovansuita@gmail.com>
2025-11-30 21:22:21 -0300

    Fix counter increment logic in MainActivity
    Updated click event handlers for TextView and Button to correctly increment and display the counter.

--- app/src/main/java/com/example/aula02/MainActivity.java
+++ app/src/main/java/com/example/aula02/MainActivity.java
@@ -9,9 +9,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Variáveis principais
     Button button;
     TextView numero;
-
     int contador = 0;
 
     @Override
@@ -19,27 +19,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Liga numero ao TextView do layout e cria um evento de clique
         numero = findViewById(R.id.textView);
         numero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView numero = (TextView) view;
+                TextView numero = (TextView) view; //Converte o elemento para TextView
                 numero.setText("+");
-                contador++;
-                numero.setText(Integer.toString(contador));
+                contador++; //Incrementa o contador
+                numero.setText(Integer.toString(contador)); //Converte o contador para texto e exibe no TextView
 
             }
         });
 
         button = findViewById(R.id.button);
         button.setText("Click aqui");
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() { //Evento do Botão
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) { // Uma vez clicado ele fica com o "+"
                 Button b = (Button) view;
                 b.setText("+");
                 contador++;
-                numero.setText(Integer.toString(contador));
+                numero.setText(Integer.toString(contador)); //Atualiza o TextView principal numero com o valor do contador
             }
         });
     }
